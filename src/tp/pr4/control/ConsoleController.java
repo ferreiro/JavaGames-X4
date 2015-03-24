cf9a3cfeb6b66f05cdaa194ddcfa874497dee506
pegartillo <pegarc03@ucm.es>
2015-03-24 17:34:08 +0100

    Addind the last classes that were left

---
+++ src/tp/pr4/control/ConsoleController.java
@@ -0,0 +1,13 @@
+package tp.pr4.control;
+
+import tp.pr4.logic.Game;
+
+public class ConsoleController {
+	public ConsoleController(GameTypeFactory factory, Game g){
+		
+	}
+	
+	public void run(){
+		
+	}
+}
---
+++ src/tp/pr4/views/window/MainWindow.java
@@ -0,0 +1,46 @@
+package tp.pr4.views.window;
+
+import javax.swing.JFrame;
+
+import tp.pr4.logic.Counter;
+import tp.pr4.logic.GameObserver;
+import tp.pr4.logic.ReadOnlyBoard;
+
+public class MainWindow extends JFrame implements GameObserver {
+
+	@Override
+	public void moveExecFinished(ReadOnlyBoard board, Counter player, Counter nextPlayer) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void moveExecStart(Counter player) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onGameOver(ReadOnlyBoard board, Counter winner) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onMoveError(String msg) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onUndo(ReadOnlyBoard board, Counter nextPlayer, boolean undoPossible) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void onUndoNotPossible() {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void reset(ReadOnlyBoard board, Counter player, Boolean undoPossible) {
+		// TODO Auto-generated method stub
+	}
+
+}
