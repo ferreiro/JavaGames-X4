5a754e3cadc64e9e70b26480fe7ff2abb659a34a
Jorge Garcia <silversoldier9@hotmail.com>
2015-04-06 23:32:19 +0200

    Clean up Resources.java

--- src/tp/pr4/Resources/Resources.java
+++ src/tp/pr4/Resources/Resources.java
@@ -1,4 +1,4 @@
-package tp.pr4.Resources;
+ package tp.pr4.Resources;
 
 import java.util.Scanner;
 
@@ -43,15 +43,15 @@ public class Resources {
 	// Which returns the first row occupied (means that is the last movement of the user in that colum)
 	
 	public static int occupiedRowPosition(Board board, int col) {
-		int height = board.getHeight();
-		int row = height, y = 1;
+		int row, height, y = 1;
 		boolean occupied = false;
 		
+		height = row = board.getHeight();
+		
 		while (!occupied && y <= height) {
-			if (board.getPosition(col, y) != Counter.EMPTY) {
+			if (board.getPosition(col, y) != Counter.EMPTY)
 				occupied = true;
 				row = y;
-			}
 			y++;
 		}
 		return row;
@@ -60,14 +60,13 @@ public class Resources {
 		
 	public static int menu(Game game, Scanner input) {
 		int option = - 1;
-		String optionString = "";
-		String lowerCaseStr;
 		boolean valid = false;
+		String optionString = "", lowerCaseStr;
 		
 		while (!valid) {
 			/*
 			 * This method is not used in the forth assigment
-			game.getBoard().printBoard();
+			 *	game.getBoard().printBoard();
 			*/
 			whoMoves(game);
 			System.out.print ("Please enter a command: ");
@@ -184,56 +183,47 @@ public class Resources {
 	}
 		
 	public static void whoMoves(Game game) {
-		if (game.getTurn() == Counter.WHITE) {
+		if (game.getTurn() == Counter.WHITE)
 			System.out.println("White to move");
-		}
-		else if (game.getTurn() == Counter.BLACK) {
+		else if (game.getTurn() == Counter.BLACK)
 			System.out.println("Black to move");
-		}
 	}
 	
-	public static void moveColumnDown(Board board, int column)
-	{
-		for (int i = board.getHeight(); i > 1; i--) {
+	public static void moveColumnDown(Board board, int column) {
+		for (int i = board.getHeight(); i > 1; i--)
 			board.setPosition(column, i, board.getPosition(column , i - 1));
-		}
 	}
-	public static void moveColumnUp(Board board, int column)
-	{
+	public static void moveColumnUp(Board board, int column) {
 		for (int i = 1 ; i < board.getHeight(); i++)
-		{
 			board.setPosition(column, i, board.getPosition(column, i + 1));
-		}
 	}
 	
 	// He tenido que escribir de nuevo la función de pedro para 
 	// que funcione el conecta 4.
 	
 	public static boolean fullColumn(int column, Board b) {
-		int row = b.getHeight();
 		boolean isFull = true;
+		int row = b.getHeight();
 		
 		while ((isFull) && (row >= 1)) {
-			if (b.getPosition(column, row) == Counter.EMPTY) {
+			if (b.getPosition(column, row) == Counter.EMPTY) 
 				isFull = false;
-			}
 			row--;
 		}
 		
 		return isFull;
 	}
 	
-	public static void setGravityDimX(int x){
+	public static void setGravityDimX(int x) {
 		DIMX_GRAVITY = x;
 	}
-	
 	public static void setGravityDimY(int y){
 		DIMY_GRAVITY = y;
 	}
 	
 	public static GravityMove applyGravity(Board board, int column, int row, Counter counter){
 		int distRight, distLeft, distUp, distBottom;
-		distLeft = column-1;
+		distLeft = column - 1;
 		distRight = DIMX_GRAVITY - column;
 		distUp = row-1;
 		distBottom = DIMY_GRAVITY - row;
@@ -244,81 +234,75 @@ public class Resources {
 		minDIM = Math.min(DIMX_GRAVITY, DIMY_GRAVITY);
 		minDimInt = (int) Math.ceil(minDIM/2);
 		
-		if((distLeft == distRight) && (distUp == distBottom)){
+		if((distLeft == distRight) && (distUp == distBottom)) {
 			movement = displaceCounter(board, column, row, 0, 0, counter);
 		}
-		else if (distLeft == distRight){
-			if (distUp < distBottom){
+		else if (distLeft == distRight) {
+			if (distUp < distBottom) {
 				movement = displaceCounter(board, column, row, 0, -1, counter);
 			}
-			else if (distBottom < distUp){
+			else if (distBottom < distUp) {
 				movement = displaceCounter(board, column, row, 0, +1, counter);
 			}
 		}
-		else if (distUp == distBottom){
-			if (distLeft < distRight){
+		else if (distUp == distBottom) {
+			if (distLeft < distRight) {
 				movement = displaceCounter(board, column, row, -1, 0, counter);
 			}
-			else if (distLeft > distRight){
+			else if (distLeft > distRight) {
 				movement = displaceCounter(board, column, row, +1, 0, counter);
 			}
 		}
-		else if ((distUp == distRight) && (distUp < minDimInt)){
+		else if ((distUp == distRight) && (distUp < minDimInt)) {
 			movement = displaceCounter(board, column, row, +1, -1, counter);
 		}
-		else if ((distUp == distLeft) && (distUp < minDimInt)){
+		else if ((distUp == distLeft) && (distUp < minDimInt)) {
 			movement = displaceCounter(board, column, row, -1, -1, counter);	
 		}
-		else if ((distBottom == distRight) && (distBottom < minDimInt)){
+		else if ((distBottom == distRight) && (distBottom < minDimInt)) {
 			movement = displaceCounter(board, column, row, +1, +1, counter);
 		}
 		else if ((distBottom == distLeft) && (distBottom < minDimInt)){
 			movement = displaceCounter(board, column, row, -1, +1, counter);
 		}
-		else if ((distUp < distRight) && (distUp < distLeft) && (distUp < distBottom)){
+		else if ((distUp < distRight) && (distUp < distLeft) && (distUp < distBottom)) {
 			movement = displaceCounter(board, column, row, 0, -1, counter);
 		}
-		else if ((distRight < distUp) && (distRight < distLeft) && (distRight < distBottom)){
+		else if ((distRight < distUp) && (distRight < distLeft) && (distRight < distBottom)) {
 			movement = displaceCounter(board, column, row, +1, 0, counter);	
 		}
-		else if ((distLeft < distRight) && (distLeft < distUp) && (distLeft < distBottom)){
+		else if ((distLeft < distRight) && (distLeft < distUp) && (distLeft < distBottom)) {
 			movement = displaceCounter(board, column, row, -1, 0, counter);
 		}
-		else if ((distBottom < distRight) && (distBottom < distLeft) && (distBottom < distUp)){
+		else if ((distBottom < distRight) && (distBottom < distLeft) && (distBottom < distUp)) {
 			movement = displaceCounter(board, column, row, 0, +1, counter);
 		}
 		return movement;
 	}
 	
-	public static GravityMove displaceCounter(Board board, int posCol, int posRow, int movCol, int movRow, Counter counter){
+	public static GravityMove displaceCounter(Board board, int posCol, int posRow, int movCol, int movRow, Counter counter) {
 		boolean ocuppy = false;
-		int actualRow, actualColumn;
-		actualRow = posRow;
-		actualColumn = posCol;
+		int actualRow = posRow, actualColumn = posCol;
 		
-		if ((movCol == 0) && (movRow == 0)){
+		if ((movCol == 0) && (movRow == 0))
 			board.setPosition(actualColumn, actualRow, counter);
-		}
 		
 		while (!ocuppy){
-			if (actualColumn > 1 && actualColumn < DIMX_GRAVITY && actualRow > 1 && actualRow < DIMY_GRAVITY){
-				if (board.getPosition(actualColumn + movCol, actualRow + movRow) != Counter.EMPTY){
+			if (actualColumn > 1 && actualColumn < DIMX_GRAVITY && actualRow > 1 && actualRow < DIMY_GRAVITY) {
+				if (board.getPosition(actualColumn + movCol, actualRow + movRow) != Counter.EMPTY) 
 					ocuppy = true;
-				}
-				else{
+				else
 					actualColumn += movCol;
 					actualRow += movRow;
-				}
 			}
-			else
-			{
+			else {
 				ocuppy = true;
 			}
 		}
 		return new GravityMove(actualColumn, actualRow, counter);	
 	}
 	
-	public static void help(){
+	public static void help() {
 		System.out.println("The available commands are:");
 		System.out.println("");
 		System.out.println("MAKE A MOVE: place a counter on the board.");
@@ -330,7 +314,7 @@ public class Resources {
 		System.out.println("HELP: show this help.");
 	}
 	
-	public static void helpInit(){
+	public static void helpInit() {
 		System.out.println("usage: tp.pr3.Main [-g <game>] [-h] [-x <columnNumber>] [-y <rowNumber>]");
 		System.out.println(" -g,--game <game>           Type of game (c4, co, gr). By default, c4.");
 		System.out.println(" -h,--help                  Displays this help.");
