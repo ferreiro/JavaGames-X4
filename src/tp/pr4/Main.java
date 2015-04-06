366929460f414e760306b695cab7a3ade053529d
Jorge Garcia <silversoldier9@hotmail.com>
2015-04-06 21:57:43 +0200

    Updating icons and main

--- src/tp/pr4/Main.java
+++ src/tp/pr4/Main.java
@@ -17,137 +17,139 @@ import tp.pr4.views.window.MainWindow;
 
 public class Main {
 
-//	public static void main(String[] args) {
-//		Game game;
-//		Scanner in = new Scanner(System.in);;
-//		Controller controller;
-//		
-//		GameRules gameRules = new Connect4Rules();
-//		GameTypeFactory f = new Connect4Factory();
-//		
-//		boolean valid = false, help = false;
-//		
-//			if (args.length == 0){
-//				valid = true;
-//			}
-//			else if (args.length == 1) {
-//				if (args[0].equals("-h") || args[0].equals("--help")){
-//					Resources.helpInit();
-//					help = true;
-//				}
-//				else{
-//					System.err.println("Incorrect use: Unrecognized option: " + args[0]);
-//					System.err.println("For more details, use -h|--help.");
-//				}
-//			}
-//			else if (args.length == 2) {
-//				if (args[0].equals("-g") || args[0].equals("--game")){
-//					if (args[1].equals("c4")){
-//						gameRules = new Connect4Rules();
-//						f = new Connect4Factory();
-//						valid = true;
-//					}
-//					else if (args[1].equals("co")){
-//						gameRules = new ComplicaRules();
-//						f = new ComplicaFactory();
-//						valid = true;
-//					}
-//					else if (args[1].equals("gr")){
-//						gameRules = new GravityRules(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
-//						f = new GravityFactory(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
-//						valid = true;
-//					}
-//					else {
-//						System.err.println("Incorrect use: game ’" + args[1].toLowerCase() + "’ incorrect.");
-//						System.err.println("For more details, use -h|--help.");
-//					}
-//				}
-//				else {
-//					System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
-//					System.err.println("For more details, use -h|--help.");
-//				}
-//			}
-//			else if (args.length == 5) {
-//				if (args[0].equals("-g") || args[0].equals("--game")){
-//					if(args[1].equals("c4") || args[1].equals("co")){
-//						System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4]);
-//						System.err.println("For more details, use -h|--help.");
-//					}
-//				}
-//			}
-//			else if (args.length == 6){
-//				if (args[0].equals("-g") || args[0].equals("--game")){
-//					if (args[1].equals("gr")){
-//						if (args[2].equals("-x") || args[2].equals("--dimX")){
-//							try {
-//								   Integer.parseInt(args[3]);
-//								   if (args[4].equals("-y") || args[4].equals("--dimY")){
-//									   try {
-//										   Integer.parseInt(args[5]);
-//										   Resources.setGravityDimX(Integer.parseInt(args[3]));
-//										   Resources.setGravityDimY(Integer.parseInt(args[5]));
-//										   gameRules = new GravityRules(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
-//										   f = new GravityFactory(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
-//										   valid = true;
-//									   }
-//									   catch(NumberFormatException e){
-//										   System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
-//										   System.err.println("For more details, use -h|--help.");
-//									   }
-//								   }
-//								   else {
-//									   System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
-//									   System.err.println("For more details, use -h|--help.");
-//								   }
-//							}
-//							catch(NumberFormatException e){
-//								System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
-//								System.err.println("For more details, use -h|--help.");
-//							}
-//						}
-//						else {
-//							System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
-//							System.err.println("For more details, use -h|--help.");
-//						}
-//					}
-//					else if(args[1].equals("c4") || args[1].equals("co")){
-//						System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
-//						System.err.println("For more details, use -h|--help.");
-//					}
-//					else{
-//						System.err.println("Incorrect use: game ’" + args[1].toLowerCase() + "’ incorrect.");
-//						System.err.println("For more details, use -h|--help.");
-//					}
-//				}
-//				else {
-//					System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
-//					System.err.println("For more details, use -h|--help.");
-//				}
-//			}
-//			else {
-//				System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
-//				System.err.println("For more details, use -h|--help.");
-//			}
-//			
-//			if(valid){
-//				game = new Game(gameRules);
-//				
-//				controller = new Controller(f, game, in);
-//				controller.run();
-//				System.exit(0);
-//			}
-//			else if (help){
-//				System.exit(0);
-//			}
-//			else{
-//				System.exit(1);
-//			}
-//		}
-//			
-//	
-	
 	public static void main(String[] args) {
+		
 		new MainWindow();
+
+/*
+ * 		PREVIOUS MAIN OF THE PROJECT
+ * 
+		Game game;
+		Scanner in = new Scanner(System.in);;
+		Controller controller;
+		
+		GameRules gameRules = new Connect4Rules();
+		GameTypeFactory f = new Connect4Factory();
+		
+		boolean valid = false, help = false;
+		
+			if (args.length == 0){
+				valid = true;
+			}
+			else if (args.length == 1) {
+				if (args[0].equals("-h") || args[0].equals("--help")){
+					Resources.helpInit();
+					help = true;
+				}
+				else{
+					System.err.println("Incorrect use: Unrecognized option: " + args[0]);
+					System.err.println("For more details, use -h|--help.");
+				}
+			}
+			else if (args.length == 2) {
+				if (args[0].equals("-g") || args[0].equals("--game")){
+					if (args[1].equals("c4")){
+						gameRules = new Connect4Rules();
+						f = new Connect4Factory();
+						valid = true;
+					}
+					else if (args[1].equals("co")){
+						gameRules = new ComplicaRules();
+						f = new ComplicaFactory();
+						valid = true;
+					}
+					else if (args[1].equals("gr")){
+						gameRules = new GravityRules(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
+						f = new GravityFactory(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
+						valid = true;
+					}
+					else {
+						System.err.println("Incorrect use: game ’" + args[1].toLowerCase() + "’ incorrect.");
+						System.err.println("For more details, use -h|--help.");
+					}
+				}
+				else {
+					System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
+					System.err.println("For more details, use -h|--help.");
+				}
+			}
+			else if (args.length == 5) {
+				if (args[0].equals("-g") || args[0].equals("--game")){
+					if(args[1].equals("c4") || args[1].equals("co")){
+						System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4]);
+						System.err.println("For more details, use -h|--help.");
+					}
+				}
+			}
+			else if (args.length == 6){
+				if (args[0].equals("-g") || args[0].equals("--game")){
+					if (args[1].equals("gr")){
+						if (args[2].equals("-x") || args[2].equals("--dimX")){
+							try {
+								   Integer.parseInt(args[3]);
+								   if (args[4].equals("-y") || args[4].equals("--dimY")){
+									   try {
+										   Integer.parseInt(args[5]);
+										   Resources.setGravityDimX(Integer.parseInt(args[3]));
+										   Resources.setGravityDimY(Integer.parseInt(args[5]));
+										   gameRules = new GravityRules(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
+										   f = new GravityFactory(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
+										   valid = true;
+									   }
+									   catch(NumberFormatException e){
+										   System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
+										   System.err.println("For more details, use -h|--help.");
+									   }
+								   }
+								   else {
+									   System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
+									   System.err.println("For more details, use -h|--help.");
+								   }
+							}
+							catch(NumberFormatException e){
+								System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
+								System.err.println("For more details, use -h|--help.");
+							}
+						}
+						else {
+							System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
+							System.err.println("For more details, use -h|--help.");
+						}
+					}
+					else if(args[1].equals("c4") || args[1].equals("co")){
+						System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
+						System.err.println("For more details, use -h|--help.");
+					}
+					else{
+						System.err.println("Incorrect use: game ’" + args[1].toLowerCase() + "’ incorrect.");
+						System.err.println("For more details, use -h|--help.");
+					}
+				}
+				else {
+					System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
+					System.err.println("For more details, use -h|--help.");
+				}
+			}
+			else {
+				System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
+				System.err.println("For more details, use -h|--help.");
+			}
+			
+			if(valid){
+				game = new Game(gameRules);
+				
+				controller = new Controller(f, game, in);
+				controller.run();
+				System.exit(0);
+			}
+			else if (help){
+				System.exit(0);
+			}
+			else{
+				System.exit(1);
+			}
+ 			*/
+		
 	}
 }
 	
