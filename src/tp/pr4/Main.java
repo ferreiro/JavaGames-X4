f14e28b2c3f9e1498315b3ccbf6221281fc01e42
pegartillo <pegarc03@ucm.es>
2015-04-17 23:41:48 +0200

    Main is completely new and much more powerful :taxi:

--- src/tp/pr4/Main.java
+++ src/tp/pr4/Main.java
@@ -1,5 +1,7 @@
 package tp.pr4;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Scanner;
 
 import tp.pr4.Resources.Resources;
@@ -15,134 +17,129 @@ import tp.pr4.logic.Connect4Rules;
 import tp.pr4.logic.Game;
 import tp.pr4.logic.GameRules;
 import tp.pr4.logic.GravityRules;
+import tp.pr4.logic.Move;
 import tp.pr4.views.console.ConsoleView;
 import tp.pr4.views.window.MainWindow;
 
 public class Main {
 
 	public static void main(String[] args) {
-		boolean valid = false, help = false, window = true;
+		boolean valid = true, help = false;
+		
+		int modeInt = 0; // 0 window, 1 console
+		int gameInt = 0; // 0 c4, 1 co, 2 gr
+		
+		
+		int counter = 0;
+		Deque<String> deque = new ArrayDeque<>();
+		
 		Scanner in = new Scanner(System.in);
 		GameRules r = new Connect4Rules();
 		Game g = new Game(r);	// The game is the model
 		GameTypeFactory factory = new Connect4Factory();
 		Controller controller;
-		 
-
-		if (args.length == 0){
-			valid = true;
+		
+		
+		for (int i = 0; i< args.length; i++){
+			deque.addLast(args[i].toLowerCase());
 		}
-		else if (args.length == 1) {
-			if (args[0].equals("-h") || args[0].equals("--help")){
+		
+		while (counter < args.length && valid){
+			if (deque.getFirst().equals("-h") || deque.getFirst().equals("--help")){
+				deque.removeFirst();
 				Resources.helpInit();
 				help = true;
 			}
-			else{
-				System.err.println("Incorrect use: Unrecognized option: " + args[0]);
-				System.err.println("For more details, use -h|--help.");
-			}
-		}
-		else if (args.length == 2) {
-			if (args[0].equals("-g") || args[0].equals("--game")){
-				if (args[1].equals("c4")){
-					r = new Connect4Rules();
-					factory = new Connect4Factory();
-					valid = true;
+			else if(deque.getFirst().equals("-g") || deque.getFirst().equals("--game")){
+				help = false;
+				deque.removeFirst();
+				if (deque.getFirst().equals("c4")){
+					deque.removeFirst();
+					gameInt = 0;
 				}
-				else if (args[1].equals("co")){
-					r = new ComplicaRules();
-					factory = new ComplicaFactory();
-					valid = true;
+				else if (deque.getFirst().equals("co")){
+					deque.removeFirst();
+					gameInt = 1;
 				}
-				else if (args[1].equals("gr")){
-					r = new GravityRules(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
-					factory = new GravityFactory(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
-					valid = true;
+				else if (deque.getFirst().equals("gr")){
+					deque.removeFirst();
+					gameInt = 2;
 				}
 				else {
-					System.err.println("Incorrect use: game ’" + args[1].toLowerCase() + "’ incorrect.");
+					System.err.println("Incorrect use: game ’" + deque.getFirst().toLowerCase() + "’ incorrect.");
 					System.err.println("For more details, use -h|--help.");
+					valid = false;
 				}
+				counter++;
 			}
-			else if(args[0].equals("-u") || args[0].equals("--ui")){
-				if (args[1].equals("console")){
-					window = false;
-					valid = true;
+			else if(deque.getFirst().equals("-u") || deque.getFirst().equals("--ui")){
+				help = false;
+				deque.removeFirst();
+				if (deque.getFirst().equals("console")){
+					deque.removeFirst();
+					modeInt = 1;
 				}
-				else if (args[1].equals("window")){
-					window = true;
-					valid = true;
+				else if (deque.getFirst().equals("window")){
+					deque.removeFirst();
+					modeInt = 0;
 				}
 				else{
-					
-				}
-			}
-			else {
-				System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
-				System.err.println("For more details, use -h|--help.");
-			}
-		}
-		else if (args.length == 5) {
-			if (args[0].equals("-g") || args[0].equals("--game")){
-				if(args[1].equals("c4") || args[1].equals("co")){
-					System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4]);
+					System.err.println("Incorrect use: Unrecognized option: " + deque.getFirst());
 					System.err.println("For more details, use -h|--help.");
+					valid = false;
 				}
+				counter++;
 			}
-		}
-		else if (args.length == 6){
-			if (args[0].equals("-g") || args[0].equals("--game")){
-				if (args[1].equals("gr")){
-					if (args[2].equals("-x") || args[2].equals("--dimX")){
-						try {
-							   Integer.parseInt(args[3]);
-							   if (args[4].equals("-y") || args[4].equals("--dimY")){
-								   try {
-									   Integer.parseInt(args[5]);
-									   Resources.setGravityDimX(Integer.parseInt(args[3]));
-									   Resources.setGravityDimY(Integer.parseInt(args[5]));
-									   r = new GravityRules(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
-									   factory = new GravityFactory(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
-									   valid = true;
-								   }
-								   catch(NumberFormatException e){
-									   System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
-									   System.err.println("For more details, use -h|--help.");
-								   }
-							   }
-							   else {
-								   System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
-								   System.err.println("For more details, use -h|--help.");
-							   }
-						}
-						catch(NumberFormatException e){
-							System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
-							System.err.println("For more details, use -h|--help.");
-						}
-					}
-					else {
-						System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
-						System.err.println("For more details, use -h|--help.");
-					}
-				}
-				else if(args[1].equals("c4") || args[1].equals("co")){
-					System.err.println("Incorrect use: illegal arguments: " + args[2] + " " + args[3] + " " + args[4] + " " + args[5]);
-					System.err.println("For more details, use -h|--help.");
-				}
-				else{
-					System.err.println("Incorrect use: game ’" + args[1].toLowerCase() + "’ incorrect.");
-					System.err.println("For more details, use -h|--help.");
-				}
+			else if(deque.getFirst().equals("-x") || deque.getFirst().equals("--dimX")){
+				help = false;
+				deque.removeFirst();
+				try {
+					   Integer.parseInt(deque.getFirst());
+					   Resources.setGravityDimX(Integer.parseInt(deque.getFirst()));
+					   deque.removeFirst();
+				   }
+				   catch(NumberFormatException e){
+					   System.err.println("Incorrect use: illegal arguments: ");
+					   System.err.println("For more details, use -h|--help.");
+					   valid = false;
+				   }
+				counter++;
 			}
-			else {
-				System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
-				System.err.println("For more details, use -h|--help.");
+			else if(deque.getFirst().equals("-y") || deque.getFirst().equals("--dimY")){
+				help = false;
+				deque.removeFirst();
+				try {
+					   Integer.parseInt(deque.getFirst());
+					   Resources.setGravityDimY(Integer.parseInt(deque.getFirst()));
+					   deque.removeFirst();
+				   }
+				   catch(NumberFormatException e){
+					   System.err.println("Incorrect use: illegal arguments: ");
+					   System.err.println("For more details, use -h|--help.");
+					   valid = false;
+				   }
+				counter++;
 			}
+			counter++;
 		}
-		else {
-			System.err.println("Incorrect use: Unrecognized option: " + args[0].toLowerCase());
-			System.err.println("For more details, use -h|--help.");
+		
+		if (gameInt == 1){
+			r = new ComplicaRules();
+			g = new Game(r);	// The game is the model
+			factory = new ComplicaFactory();
 		}
+		else if (gameInt == 2){
+			r = new GravityRules(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
+			g = new Game(r);	// The game is the model
+			factory = new GravityFactory(Resources.DIMX_GRAVITY, Resources.DIMY_GRAVITY);
+		}
+		
+		if (modeInt == 0)
+			controller = new WindowController(factory, g);
+		else
+			controller = new ConsoleController(factory, g, in);
+		
+
 		
 		if(valid) {
 			g = new Game(r);
@@ -151,10 +148,6 @@ public class Main {
 			g.addObserver(new ConsoleView(g));
 			g.addObserver(new MainWindow(factory, g));
 			
-			if (window)
-				controller = new WindowController(factory, g);
-			else
-				controller = new ConsoleController(factory, g, in);
 			
 			controller.run();
 //				System.exit(0);
