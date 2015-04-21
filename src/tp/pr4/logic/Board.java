70b06de65046424b898a3d3e434f5e721d708d91
pegartillo1995 <pegarc03@ucm.es>
2015-04-21 10:38:35 +0200

    No exceptions in connect4

--- src/tp/pr4/logic/Board.java
+++ src/tp/pr4/logic/Board.java
@@ -51,18 +51,21 @@ public class Board implements ReadOnlyBoard {
 	public void setPosition(int tx, int ty, Counter counter) {
 		
 		//DEBUG
-		System.out.println("tx" + (tx-1));
-		System.out.println("ty" + (ty-1));
+		System.out.println("tx" + (tx));
+		System.out.println("ty" + (ty));
 		System.out.println("dimX" + width);
 		System.out.println("dimY" + height);
 		
 		
 		if ((tx >= 1 && tx <= width) && (ty >= 1 && ty <= height)) 
-			if (board[tx-1][ty-1] == Counter.EMPTY && counter != Counter.EMPTY)
+			if (board[ty-1][tx-1] == Counter.EMPTY && counter != Counter.EMPTY) {
 				occupiedCells++;
-			else if (board[tx-1][ty-1] != Counter.EMPTY && counter == Counter.EMPTY)
+				board[ty - 1][tx - 1] = counter;//cambiado x por y
+			}
+			else if (board[ty-1][tx-1] != Counter.EMPTY && counter == Counter.EMPTY) {
 				occupiedCells--;
-			board[ty - 1][tx - 1] = counter;
+				board[ty - 1][tx - 1] = counter;//cambiado x por y
+			}
 	}
 	
 	// Methods for checking if a board neither column is full or not.
