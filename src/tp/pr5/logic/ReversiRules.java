1018663892183af9ae52479742725fd2eca77e0b
Jorge Garcia <silversoldier9@hotmail.com>
2015-04-28 21:36:57 +0200

    [REVERSI] Creating winningMOve and isDraw algorithms

--- src/tp/pr5/logic/ReversiRules.java
+++ src/tp/pr5/logic/ReversiRules.java
@@ -11,39 +11,52 @@ public class ReversiRules implements GameRules {
 	public ReversiRules() {
 		winner = Counter.EMPTY;
 	}
+ 
+	public Counter initialPlayer() {
+		return Counter.BLACK;
+	}
 
+	/**************************
+	 	Winning move
+	***************************/
+	
 	public Counter winningMove(Move lastMove, Board b) {
-		boolean valid = false;
-		Counter c = Counter.EMPTY;
+		Counter colorWinner = Counter.EMPTY;
+		int totalWhite, totalBlack;
 
-		// Check if isFull(), no body wons
+		if (b.isFull()) { // Si está lleno el tablero, vemos qué color tiene más fichas
+			totalWhite = countTiles(Counter.WHITE, b);
+			totalBlack = countTiles(Counter.BLACK, b);
+			
+			if (totalWhite < totalBlack) {
+				colorWinner = Counter.BLACK;
+			}
+			else if (totalWhite > totalBlack) {
+				colorWinner = Counter.WHITE;	// Hay más fichas BLANCAS! Gana
+			} 
+		}
 		
-		return c;
+		return colorWinner;
 	}
-
 	
+	// Return total number of appearances of a Color 
 	
-	
-	// TODO: Aquí faltan los algoritmos para ver si aluien ha gando o no
-	
-	
-	
-	
-	
-	public Board newBoard() {
-		Board b = new Board(dimX, dimY);
-		int initX = dimX / 2, initY = dimY / 2;
-
-		// Initializing board with tiles
-		
-		b.setPosition(initX, initX+1, Counter.WHITE);
-		b.setPosition(initX, initY, Counter.BLACK);
-		b.setPosition(initX+1, initY+1, Counter.BLACK);
-		b.setPosition(initX+1, initY, Counter.WHITE);
-		
-		return b;
+	public int countTiles(Counter c, Board b) { 
+		int total = 0; 
+		for(int i = 0; i < dimX; i++) {
+			for(int j = 0; j < dimX; j++) {
+				if (b.getPosition(i, j) == c) 
+					total++;
+			}
+		}
+		return total;
 	}
+	
 
+	/**************************
+	 	Other functions
+	***************************/
+	
 	public boolean isDraw(Counter lastMove, Board b) {
 		boolean isDraw = false;
 		
@@ -56,26 +69,40 @@ public class ReversiRules implements GameRules {
 		 
 		return isDraw;
 	}
-
-	public Counter initialPlayer() {
-		return Counter.BLACK;
+	
+	public Board newBoard() { // Creates a Board with some tiles on the center
+		Board b = new Board(dimX, dimY);
+		int initX = dimX / 2, initY = dimY / 2;
+ 
+		b.setPosition(initX, initX+1, Counter.WHITE); // Initializing board with tiles
+		b.setPosition(initX, initY, Counter.BLACK);
+		b.setPosition(initX+1, initY+1, Counter.BLACK);
+		b.setPosition(initX+1, initY, Counter.WHITE);
+		
+		return b;
 	}
 
 	public Counter nextTurn(Counter lastMove, Board b) {
 		Counter nextTurn = Counter.EMPTY;
 		
-		if (lastMove == Counter.WHITE) {
-			nextTurn = Counter.BLACK;
-		}
-		else if (lastMove == Counter.BLACK) {
-			nextTurn = Counter.WHITE;
-		}
+		if (lastMove == Counter.WHITE) nextTurn = Counter.BLACK;
+		else if (lastMove == Counter.BLACK) nextTurn = Counter.WHITE; 
 
 		return nextTurn;
 	}
 
-	public int getDimX() {return this.dimX;}
-	public int getDimY() {return this.dimY;}
-	public int intRules() {return 3;}
+	/**************************
+	 	Getters and setters 
+	***************************/
+	
+	public int getDimX() {
+		return this.dimX;
+	}
+	public int getDimY() {
+		return this.dimY;
+	}
+	public int intRules() {
+		return 3;
+	}
 
 }
