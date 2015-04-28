58bddfd24377c8fce685b7e82e7561e3658474f4
Jorge Garcia <silversoldier9@hotmail.com>
2015-04-28 18:43:07 +0200

    Deleting old algorithm on REVERSI

--- src/tp/pr5/logic/ReversiRules.java
+++ src/tp/pr5/logic/ReversiRules.java
@@ -11,39 +11,6 @@ public class ReversiRules implements GameRules {
 	public ReversiRules() {
 		winner = Counter.EMPTY;
 	}
-	
-	// Horizontal Algorithm	
-	/*
-	public boolean checkHorizontal(Board b, int x, int y, boolean left) {
-		boolean valid = false;
-		int auxColumn = x, total = 0;
-		Counter color = b.getPosition(x, y);
-		
-		if (left) { // Check left tiles
-			while((auxColumn > 1) && (b.getPosition(auxColumn - 1, y) != color)) {
-				total += 1; auxColumn -= 1; 
-			}
-			if ((total >= 1) && (b.getPosition(auxColumn - 1, y) == color)) {
-				valid = true; // If there is unless one Counter of the other color and the last counter is the same as the move color
-				// Si se han formado celdas y la última ficha es del mismo color que la primera 
-			}
-		} 
-		else { // Check Right tiles
-			while((auxColumn < b.getWidth()) && (b.getPosition(auxColumn + 1, y) != color)) {
-				total += 1; auxColumn += 1;	// When right, move to right
-			}
-			if ((total >= 1) && (b.getPosition(auxColumn, y) == color)) {
-				valid = true; // If there is unless one Counter of the other color and the last counter is the same as the move color
-			}
-		}
-		if (valid) {
-			// TODO: Marcar las celdas
-			// Posición final para guardar: (auxColumn, y)
-		}
-	 
-		return valid;
-	}
-	*/
 
 	public Counter winningMove(Move lastMove, Board b) {
 		boolean valid = false;
