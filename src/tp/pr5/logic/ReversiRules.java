e1fbb61941c6301cf12ddb2fbbc9d23678e1d314
Jorge Garcia <silversoldier9@hotmail.com>
2015-04-28 18:07:49 +0200

    Improving and fixing possible erros on ReversiRules algorithms

--- src/tp/pr5/logic/ReversiRules.java
+++ src/tp/pr5/logic/ReversiRules.java
@@ -12,36 +12,66 @@ public class ReversiRules implements GameRules {
 		winner = Counter.EMPTY;
 	}
 	
-	// Horizontal Algorithm
-
-	public boolean horizontalFormed(Board b, int x, int y) {
+	// Horizontal Algorithm	
+	/*
+	public boolean checkHorizontal(Board b, int x, int y, boolean left) {
 		boolean valid = false;
-		int total = 0, auxColumn = x; // Copy the current column
-		Counter currentColor = b.getPosition(x, y);
+		int auxColumn = x, total = 0;
+		Counter color = b.getPosition(x, y);
 		
-		// Check Left titles
-		while((auxColumn >= 1) && (b.getPosition(auxColumn - 1, y) != currentColor)) {
-			total++;			
-			auxColumn -= 1;
+		if (left) { // Check left tiles
+			while((auxColumn > 1) && (b.getPosition(auxColumn - 1, y) != color)) {
+				total += 1; auxColumn -= 1; 
+			}
+			if ((total >= 1) && (b.getPosition(auxColumn - 1, y) == color)) {
+				valid = true; // If there is unless one Counter of the other color and the last counter is the same as the move color
+				// Si se han formado celdas y la última ficha es del mismo color que la primera 
+			}
+		} 
+		else { // Check Right tiles
+			while((auxColumn < b.getWidth()) && (b.getPosition(auxColumn + 1, y) != color)) {
+				total += 1; auxColumn += 1;	// When right, move to right
+			}
+			if ((total >= 1) && (b.getPosition(auxColumn, y) == color)) {
+				valid = true; // If there is unless one Counter of the other color and the last counter is the same as the move color
+			}
 		}
-		if (total >= 1 && (auxColumn >= 1)) {
-			// Si se han formado celdas y la ficha última es de otro color
-			valid = true;
-			// MARCAR CELDAS O GUARDAR LA POSICIÓN FINAL EN UN ARRAY
-			// Posición final: (auxColumn, y)
+		if (valid) {
+			// TODO: Marcar las celdas
+			// Posición final para guardar: (auxColumn, y)
 		}
-		 
-		// Check Right titles
-		total = 0; 
-		auxColumn = x;
-
-		// swhile(auxColumn <= b.getWidth() && )
-		
+	 
 		return valid;
 	}
+	*/
+
+	public Counter winningMove(Move lastMove, Board b) {
+		boolean valid = false;
+		Counter c = Counter.EMPTY;
+		
+		// valid = checkHorizontal(b, lastMove.column, lastMove.getRow(), false);
+ 
+		
+		return c;
+	}
+
+	public boolean checkHorizontal(Board b, int x, int y, boolean left) {
+		boolean valid = false;
+		int auxColumn = x, total = 0;
+		Counter color = b.getPosition(x, y);
+		 
+		while((auxColumn > 1) && (b.getPosition(auxColumn - 1, y) != color)) {
+			total += 1; auxColumn -= 1; 
+		}
+		if ((total >= 1) && (b.getPosition(auxColumn - 1, y) == color)) {
+			valid = true; // If there is unless one Counter of the other color and the last counter is the same as the move color
+			// Si se han formado celdas y la última ficha es del mismo color que la primera 
+			// TODO: Marcar las celdas
+			// Posición final para guardar: (auxColumn, y)
+		}
 	 
-	
-	
+		return valid;
+	}
 	
 	
 	
@@ -61,11 +91,6 @@ public class ReversiRules implements GameRules {
 	public boolean isDraw(Counter lastMove, Board b) {
 		// TODO Auto-generated method stub
 		return false;
-	}
-
-	public Counter winningMove(Move lastMove, Board b) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 	public Counter initialPlayer() {
