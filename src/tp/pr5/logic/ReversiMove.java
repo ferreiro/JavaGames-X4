699607a79c6cf342a185c6e22d1badd66adf9901
Jorge Garcia <silversoldier9@hotmail.com>
2015-04-28 20:45:01 +0200

    [REVERSI] Creating diagonal algorithms

--- src/tp/pr5/logic/ReversiMove.java
+++ src/tp/pr5/logic/ReversiMove.java
@@ -13,17 +13,16 @@ public class ReversiMove extends Move {
 	public boolean executeMove(Board b) throws InvalidMove {
 		boolean valid = false;
 		
-		if ((column >= 1 && column <= Resources.DIMX_REVERSI) && (row >= 1 && row <= Resources.DIMY_REVERSI)) {
-			if (b.getPosition(column, row) == Counter.EMPTY) {
-				valid = true;
+		if ((column >= 1 && column <= Resources.DIMX_REVERSI) && 
+			(row >= 1 && row <= Resources.DIMY_REVERSI) && 
+			(b.getPosition(column, row) == Counter.EMPTY)) { 
 				//TODO: terminnar execute move
 				
 				valid = checkHorizontal(b, column, row, true); // True = left Part
-				valid = checkHorizontal(b, column, row, false); // False = right part
-				valid = checkVertical(b, column, row, true); // True = Up part
-				valid = checkVertical(b, column, row, false); // True = Up part	
+					if(!valid) valid = checkHorizontal(b, column, row, false); // False = right part
+						if (!valid) valid = checkVertical(b, column, row, true); // True = Up part
+							if (!valid) valid = checkVertical(b, column, row, false); // True = Up part	
 				
-			}
 		}	
 		 
 		return valid;
@@ -64,7 +63,7 @@ public class ReversiMove extends Move {
 		return valid;
 	}
 
-	// CheckVertical: Function to Check left and right colors given a fixed position
+	// CheckVertical: Function to Check top and bottom colors given a fixed position
 
 	public boolean checkVertical(Board b, int x, int y, boolean up) {
 		boolean valid = false;
@@ -95,6 +94,41 @@ public class ReversiMove extends Move {
 				// Posición final para guardar: (auxColumn, y)
 			}			
 		} 
+		return valid;
+	}
+	
+	// CheckHorizontal: Function to Check diagonals given a fixed position
+	
+	public boolean checkDiagonal(Board b, int x, int y, boolean topLeft) {
+		boolean valid = false;
+		int auxColumn = x, auxRow = x, total = 0;
+		Counter color = b.getPosition(x, y);
+ 
+		if (topLeft) {
+			while((auxColumn > 1) && (auxRow > 1) && (b.getPosition(auxColumn - 1, auxRow - 1) != color)) {
+				total += 1; auxColumn -= 1; auxRow -= 1; 
+			} 
+			if ((total >= 1) && (b.getPosition(auxColumn - 1, auxRow - 1) == color)) {
+				System.out.println("Valid move!"); // DEBUG
+				valid = true; // If there is unless one Counter of the other color and the last counter is the same as the move color
+				// TODO Si se han formado celdas y la última ficha es del mismo color que la primera 
+				// TODO: Marcar las celdas
+				// Posición final para guardar: (auxColumn, y)
+			}
+		}
+		else {
+			while((auxColumn < b.getWidth()) && (auxRow < b.getHeight()) && (b.getPosition(auxColumn + 1, auxRow + 1) != color)) {
+				total += 1; auxColumn += 1; auxRow += 1; 
+			} 
+			if ((total >= 1) && (b.getPosition(auxColumn + 1, auxRow + 1) == color)) {
+				System.out.println("Valid move!"); // DEBUG
+				valid = true; // If there is unless one Counter of the other color and the last counter is the same as the move color
+				// TODO Si se han formado celdas y la última ficha es del mismo color que la primera 
+				// TODO: Marcar las celdas
+				// Posición final para guardar: (auxColumn, y)
+			}			
+		}
+ 
 		return valid;
 	}
 	
