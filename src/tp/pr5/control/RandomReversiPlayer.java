2010625c4d0f5a9524227fedcbe88deff78f6a28
Jorge Garcia <silversoldier9@hotmail.com>
2015-05-02 15:37:24 +0200

    Updating RandomReversi Move

--- src/tp/pr5/control/RandomReversiPlayer.java
+++ src/tp/pr5/control/RandomReversiPlayer.java
@@ -14,26 +14,32 @@ public class RandomReversiPlayer implements Player {
 		ReversiMove m = null;
 		int column = 0, row = 0;
 		
-		while(!moved){
-			do { 
-				row    = (int) ((Math.random() * Resources.DIMY_REVERSI) + 1);
-				column = (int) ((Math.random() * Resources.DIMX_REVERSI) + 1);
-				 
-				if (board.getPosition(column, row) == Counter.EMPTY) {
-					valid = true;
-				}
-			}  while(!valid);
-			/*
-			m = new ReversiMove(column, row, counter);
-			m.checkHorizontal(board);
-			m.checkVertical(board);
-			m.checkDiagonal1(board);
-			m.checkDiagonal2(board);
-			if(m.getListLength() > 0){
-				moved = true;
+		do {
+			row    = (int) ((Math.random() * Resources.DIMY_REVERSI) + 1);
+			column = (int) ((Math.random() * Resources.DIMX_REVERSI) + 1);
+			 
+			if (board.getPosition(column, row) == Counter.EMPTY) {
+				
+				int total = 0;
+				boolean keepMove = false;
+
+				m = new ReversiMove(column, row, counter);
+				
+				total += m.checkHorizontal(board, column, row, true, keepMove);	// True  = Left
+				total += m.checkHorizontal(board, column, row, false, keepMove);	// False = Right
+				total += m.checkVertical(board, column, row, true, keepMove); 	// True  = UP	
+				total += m.checkVertical(board, column, row, false, keepMove); 	// False = Down	
+				total += m.checkDiagonal1(board, column, row, true, keepMove); 	// True = Top Left
+				total += m.checkDiagonal1(board, column, row, false, keepMove);  // False = Bottom Right
+				total += m.checkDiagonal2(board, column, row, true, keepMove); 	// True = Top Right
+				total += m.checkDiagonal2(board, column, row, false, keepMove);  // False = Bottom Left
+
+				if (total >= 1) { // => Si se ha formado al menos una check, significa que hay alguna celda flrmDa, por tanto, el movimiento es valido
+					valid = true;	 
+				}	 
 			}
-			*/
-		}
+		} while(!valid);
+		 
 		return m;
 	}
 }
