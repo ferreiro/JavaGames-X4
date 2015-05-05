5c54281036c0af05d07c09ca41c9ecd3d873a520
pegartillo <pegarc03@ucm.es>
2015-05-05 15:17:08 +0200

    Forget previous commit i have fixed the problems in wController i think they were causes by a fail copy

--- src/tp/pr5/control/WindowController.java
+++ src/tp/pr5/control/WindowController.java
@@ -6,7 +6,7 @@ import tp.pr5.logic.Game;
 import tp.pr5.logic.GameType;
 import tp.pr5.logic.InvalidMove;
 import tp.pr5.logic.Move;
-import tp.pr5.logic.PlayerType;
+import tp.pr5.logic.PlayerType;		
  
 public class WindowController extends Controller {
 	static java.util.Scanner in;
@@ -58,7 +58,7 @@ public class WindowController extends Controller {
 	}
 	
 	public void run() {
-		automaticMove();
+		// Nothing here.
 	}
 	
 	public void undo() {
@@ -101,69 +101,4 @@ public class WindowController extends Controller {
 			}
 		};
 	}
-}
-nds Controller {
-	static java.util.Scanner in;
-	
-	public WindowController(GameTypeFactory factory, Game g) {
-		super(factory,g, in);
-	}
-	
-	@SuppressWarnings("static-access")
-	public void changeGame(GameType gameType, int dimX, int dimY) {
-		if (gameType == gameType.gravity) {
-			Resources.setGravityDimX(dimX); 
-			Resources.setGravityDimY(dimY);
-		}
-		changeG(gameType, dimX, dimY);	
-	}
-	
-	public void makeMove(int col, int row, Counter turn) {
-		boolean valid;
-	
-		try {
-			Move mov = getGameTypeFactory().createMove(col, row, turn);
-			valid = game.executeMove(mov);
-			if (valid) this.changePlayer();
-		}
-		catch (InvalidMove e) {
-			System.out.println(e.getMessage());
-		}
-	}
-	
-	public void randomMove() {
-		Move rMove = getGameTypeFactory().createRandomPlayer().getMove(getGame().getBoard(), getGame().getTurn());
-		try {
-			getGame().executeMove(rMove);
-		} catch (InvalidMove e) {
-			// nothing to do
-		}
-	}
-	
-	// Quit the application.
-	public void requestQuit() {
-		game.closeGame();
-	}
-	
-	public void reset() {
-		initGame();	// Reset players, current player and 
-		game.resetGame(); // Notify the window that the reset is done
-	}
-	
-	public void run() {
-		// Nothing here.
-	}
-	
-	public void undo() {
-		boolean undo = game.undo();
-		if (undo){
-			changePlayer(); // Change Current player 
-		}
-	}
-	
-	public void setPlayerMode(Counter player, PlayerType mode){
-		player.setMode(mode);
-		//automaticMove();
-	}
-	
 }
