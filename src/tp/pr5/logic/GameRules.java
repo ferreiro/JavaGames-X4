f8c613e2f87903c41c1ccf6489eeefe3c32ed7e8
Jorge Garcia <silversoldier9@hotmail.com>
2015-06-24 04:43:53 +0200

    Cleaning GameRules interface

--- src/tp/pr5/logic/GameRules.java
+++ src/tp/pr5/logic/GameRules.java
@@ -1,29 +1,18 @@
 package tp.pr5.logic;
   
 public interface GameRules {
-	
-	// Build a board that is to be used in the game, according to the rules of that game.
-	public Board newBoard();
- 	
-	// Consulta si hay empate. tablas(Ficha ultimoEnPoner, Tablero t) 
-	
-	public boolean isDraw(Counter lastMove, Board b);
-								/* ^ LastMove?? Puede ser para actualizar el tablero con ese movimiento? */
+		
+	public Board newBoard(); // Build a board that is to be used in the game, according to the rules of that game.
+	public Counter initialPlayer();	// Says which is the firt player to move
 
-	// Checks whether or not, with the current board, one of the players has won and, if so, returns the colour of the winner
-	public Counter winningMove(Move lastMove, Board b);
-	
-	// jugadorInicial()
-	public Counter initialPlayer();
-	
-	// Returns the colour of the player whose turn it is.
-	public Counter nextTurn(Counter lastMove, Board b);
-	
-	// Getters and setters
-	// Creemos que 
-	public int getDimX();
-	public int getDimY();
-	public int intRules();
-	
+	// Getters and setters 
+	public int getDimX();	// Get with of the Board.
+	public int getDimY();	// Get height of the Board.
+	public int intRules();	// 0 = Connect, 1 = Complica, 2 = Gravity, 3 = Reversi
+
+	// Other methods
+	public boolean isDraw(Counter lastMove, Board b); // Consulta si hay empate. tablas(Ficha ultimoEnPoner, Tablero t) 
+	public Counter winningMove(Move lastMove, Board b); // Checks whether or not, with the current board, one of the players has won and, if so, returns the colour of the winner
+	public Counter nextTurn(Counter lastMove, Board b); // Returns the colour of the player whose turn it is.
 	
 }
