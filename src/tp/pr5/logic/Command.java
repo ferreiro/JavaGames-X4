ffa6d09d34d407c169a910a1145326fd87f497ee
Jorge Garcia <silversoldier9@hotmail.com>
2015-06-24 08:45:39 +0200

    Deleting unused Command.java on logic

--- src/tp/pr5/logic/Command.java
+++
@@ -1,8 +0,0 @@
-package tp.pr5.logic;
-
-import tp.pr5.control.Controller;
-
-public interface Command {
-	public void execute(Game g, Controller controller);
-	public Command parse(String line);
-}
