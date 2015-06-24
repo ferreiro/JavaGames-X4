d87b823393edac0dc9f0318a2f79d235435feef7
Jorge Garcia <silversoldier9@hotmail.com>
2015-06-24 05:30:33 +0200

    Cleaning Reversi Rules

--- src/tp/pr5/logic/ReversiRules.java
+++ src/tp/pr5/logic/ReversiRules.java
@@ -2,69 +2,57 @@ package tp.pr5.logic;
 
 import tp.pr5.Resources.Resources;
 
+@SuppressWarnings("unused")
 public class ReversiRules implements GameRules {
 
 	private int dimX = Resources.DIMX_REVERSI;
 	private int dimY = Resources.DIMY_REVERSI;
 	private Counter winner;
-	
+
+	/*************************************/
+	/********* CONSTRUCTOR ***************/
+	/*************************************/
+
 	public ReversiRules() {
 		winner = Counter.EMPTY;
 	}
- 
-	public Counter initialPlayer() {
-		return Counter.BLACK;
-	}
-
-	/**************************
-	 	Winning move
-	 * @throws InvalidMove 
-	***************************/
 	
-	public Counter winningMove(Move lastMove, Board b) {
-		Counter colorWinner = Counter.EMPTY;
-		int totalWhite, totalBlack;
-		  
-		if (isGameOver(b)) { // Si esta lleno el tablero o ya no hay movimientos disponibles para el blanco o negro...
- 			
-			totalWhite = countTiles(Counter.WHITE, b);
-			totalBlack = countTiles(Counter.BLACK, b);
-			
-			if (totalWhite < totalBlack) {
-				colorWinner = Counter.BLACK; 
-			}
-			else if (totalWhite > totalBlack) {
-				colorWinner = Counter.WHITE;	// Hay más fichas BLANCAS! Gana
-			}  		
-		}
-		winner = colorWinner;
-		return colorWinner;
-	}
-	
-	// Return total number of appearances of a Color 
-	
-	public int countTiles(Counter c, Board b) { 
-		int total = 0; 
+	public Board newBoard() { 
+		int initX = dimX / 2, initY = dimY / 2;
+		Board b = new Board(dimX, dimY);
 		
-		for(int i = 1; i <= dimX; i++) {
-			for(int j = 1; j <= dimX; j++) {
-				if (b.getPosition(i, j) == c) 
-					total++;
-			}
-		}
-
-		return total;
-	} 
-
-	private boolean isGameOver(Board b) {
-		return (b.isFull()) || (!Resources.canMakeMove(b, Counter.BLACK) && 
-				!Resources.canMakeMove(b, Counter.WHITE));
+		// Set tiles in the middle of the board
+		
+		b.setPosition(initX, initX+1, Counter.WHITE); // Initializing board with tiles
+		b.setPosition(initX, initY, Counter.BLACK);
+		b.setPosition(initX+1, initY+1, Counter.BLACK);
+		b.setPosition(initX+1, initY, Counter.WHITE);
+		
+		return b;
 	}
 	
-	/**************************
-	 	Other functions
-	***************************/
+	public Counter initialPlayer() {
+		return Counter.BLACK;	// The first player now is black
+	}
+
+	/*************************************/
+	/********* GETTERS / SETTERS *********/
+	/*************************************/
 	
+	public int getDimX() {
+		return this.dimX;
+	}
+	public int getDimY() {
+		return this.dimY;
+	}
+	public int intRules() {
+		return 3;
+	}
+
+	/*************************************/
+	/********* OTHER METHODS *************/
+	/*************************************/
+
 	public boolean isDraw(Counter lastMove, Board b) {
 		boolean isDraw = false;
 		
@@ -81,18 +69,6 @@ public class ReversiRules implements GameRules {
 		return isDraw;
 	}
 	
-	public Board newBoard() { // Creates a Board with some tiles on the center
-		Board b = new Board(dimX, dimY);
-		int initX = dimX / 2, initY = dimY / 2;
- 
-		b.setPosition(initX, initX+1, Counter.WHITE); // Initializing board with tiles
-		b.setPosition(initX, initY, Counter.BLACK);
-		b.setPosition(initX+1, initY+1, Counter.BLACK);
-		b.setPosition(initX+1, initY, Counter.WHITE);
-		
-		return b;
-	}
-
 	public Counter nextTurn(Counter lastMove, Board b) {
 		if (lastMove == Counter.EMPTY) {
 			return Counter.EMPTY;
@@ -109,18 +85,43 @@ public class ReversiRules implements GameRules {
 		}
 	}
 
-	/**************************
-	 	Getters and setters 
-	***************************/
+	public Counter winningMove(Move lastMove, Board b) {
+		Counter colorWinner = Counter.EMPTY;
+		int totalWhite, totalBlack;
+		  
+		if (isGameOver(b)) { // Si esta lleno el tablero o ya no hay movimientos disponibles para el blanco o negro...
+ 			
+			totalWhite = countTiles(Counter.WHITE, b);
+			totalBlack = countTiles(Counter.BLACK, b);
+			
+			if (totalWhite < totalBlack) {
+				colorWinner = Counter.BLACK; 
+			}
+			else if (totalWhite > totalBlack) {
+				colorWinner = Counter.WHITE;	// Hay mÃ¡s fichas BLANCAS! Gana
+			}  		
+		}
+		winner = colorWinner;
+		return colorWinner;
+	}
 	
-	public int getDimX() {
-		return this.dimX;
-	}
-	public int getDimY() {
-		return this.dimY;
-	}
-	public int intRules() {
-		return 3;
-	}
+	/********* AUXILIAR METHOFS *************/
 
+	private boolean isGameOver(Board b) {
+		return (b.isFull()) || (!Resources.canMakeMove(b, Counter.BLACK) && 
+				!Resources.canMakeMove(b, Counter.WHITE));
+	}
+	
+	public int countTiles(Counter c, Board b) { 
+		int total = 0; 
+		
+		for(int i = 1; i <= dimX; i++) {
+			for(int j = 1; j <= dimX; j++) {
+				if (b.getPosition(i, j) == c) 
+					total++;
+			}
+		}
+		return total; // Total number of tiles for a given Color 
+	} 
+ 
 }
