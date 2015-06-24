124e734c750dd801de0df551df1c67360a0c9a16
Jorge Garcia <silversoldier9@hotmail.com>
2015-06-24 07:49:47 +0200

    Simple change on CommandSet.java

--- src/tp/pr5/command/CommandSet.java
+++ src/tp/pr5/command/CommandSet.java
@@ -1,7 +1,16 @@
 package tp.pr5.command;
 
 public class CommandSet {
-	static Command[] cmds = {new UndoCmd(), new RestartCmd(), new ExitCmd(), new HelpCmd(), new MoveCmd(), new ChangeGameCmd(), new ChangePlayerCmd()};
+	
+	static Command[] cmds = {
+		new UndoCmd(), 
+		new RestartCmd(), 
+		new ExitCmd(), 
+		new HelpCmd(), 
+		new MoveCmd(), 
+		new ChangeGameCmd(), 
+		new ChangePlayerCmd()
+	};
 	
 	public static Command parse(String line){
 		for( Command c : cmds) {
